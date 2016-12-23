5b9fc30aef2bdae9ec38019a064ba13f61a46f86
shwetank-kumar <shwetank.kumar@gmail.com>
2016-12-23 11:22:40 +0530

    Added dockerfile

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM ubuntu:16.04
+
+#MAINTANER Shwetank Kumar "shwetank.kumar@gmail.com"
+
+RUN apt-get update -y && \
+    apt-get install -y python-pip python-dev
+
+RUN apt-get install -y postgresql postgresql-server-dev-9.5
+
+# We copy just the requirements.txt first to leverage Docker cache
+COPY ./requirements.txt /app/requirements.txt
+
+WORKDIR /app
+
+RUN pip install -r requirements.txt
+
+COPY . /app
+
+ENTRYPOINT [ "python" ]
+
+CMD [ "application.py" ]
--- application.py
+++ application.py
@@ -8,14 +8,19 @@ from collections import Counter
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask import render_template, request
+from rq import Queue
+from rq.job import Job
 from stop_words import stop_wds
+from worker import conn
 
 application = Flask(__name__)
 
 
 application.config.from_object(os.environ['APP_SETTINGS'])
-application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(application)
+
+q = Queue(connection=conn)
 
 from models import Result
 
@@ -25,33 +30,52 @@ nltk.data.path.append('./nltk_data/')
 
 @application.route('/', methods=['GET', 'POST'])
 def index():
-    errors = []
     results = {}
     if request.method == 'POST':
+        url = request.form['url']
+        if 'http://' not in url[:7]:
+            url = 'http://' + url
+
+        job = q.enqueue_call(func=count_and_save_words, args=(url, ), result_ttl=5000)
+        print(job.get_id())
+
+    return render_template('index.html', results=results)
+
+
+
+
+
+
+def count_and_save_words(url):
+    errors = []
+
+    try:
+        r = requests.get(url)
+    except:
+        errors.append('Could not open the URL')
+
+    if r:
+        raw_text = BeautifulSoup(r.text, 'html.parser').get_text()
+        tokens = nltk.word_tokenize(raw_text)
+        text = nltk.Text(tokens)
+        non_punct = re.compile('.*[A-Za-z].*')
+        raw_words = [w for w in text if non_punct.match(w)]
+        word_count = Counter(raw_words)
+        no_stop_words = [w for w in raw_words if w.lower() not in stop_wds]
+        no_stop_words_count = Counter(no_stop_words)
+        # results = sorted(no_stop_words_count.items(), key=operator.itemgetter(1), reverse=True)
         try:
-            url = request.form['url']
-            r = requests.get(url)
+            result = Result(url=url, result_all=word_count, result_no_stop_words=no_stop_words_count)
+            db.session.add(result)
+            db.session.commit()
+            return result.id
         except:
-            errors.append('Could not open the URL')
+            errors.append('Could not add result to database.')
+            return errors
 
-        if r:
-            raw_text = BeautifulSoup(r.text, 'html.parser').get_text()
-            tokens = nltk.word_tokenize(raw_text)
-            text = nltk.Text(tokens)
-            non_punct = re.compile('.*[A-Za-z].*')
-            raw_words = [w for w in text if non_punct.match(w)]
-            word_count = Counter(raw_words)
-            no_stop_words = [w for w in raw_words if w.lower() not in stop_wds]
-            no_stop_words_count = Counter(no_stop_words)
-            results = sorted(no_stop_words_count.items(), key=operator.itemgetter(1), reverse=True)
-            try:
-                result = Result(url=url, result_all=word_count, result_no_stop_words=no_stop_words_count)
-                db.session.add(result)
-                db.session.commit()
-            except:
-                errors.append('Could not add result to database.')
 
-    return render_template('index.html', results=results, errors=errors)
+
+
 
 
 if __name__ == '__main__':
