4d3f48dce4a6a5d5106bbf632ce72a0aa17cfc41
shwetank-kumar <shwetank.kumar@gmail.com>
2017-03-18 03:41:21 -0700

    removing straggler files

--- start_celery.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker build -f ./Dockerfile.celery -t celery .
-
-# docker build . -t 'celery'
-docker run --name celery-worker celery
--- start_redis.sh
+++
@@ -1,2 +0,0 @@
-# -d -p allows to port host ip to docker
-docker run -d -p 6379:6379 --name redis-server -d redis
