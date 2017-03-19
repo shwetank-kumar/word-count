be94cf83740a57696538d5da8602c3f984d51b4b
shwetank-kumar <shwetank.kumar@gmail.com>
2017-03-18 21:50:45 -0700

    Updated __init__.py in wordcounter

--- docker_scripts.sh
+++ docker_scripts.sh
@@ -1,71 +1,81 @@
 #!/bin/bash
+# Create network to which all services connect
 function create_network()
 {
   docker network create redis-celery
 }
 
+# Start redis server
 function start_redis()
 {
   docker run -d -p 6379:6379 --name redis-server --net=redis-celery redis
-  # docker run -d -p 6379:6379 --name redis-server redis
 }
 
+# Stop redis server
 function stop_redis()
 {
   docker stop redis-server
 }
 
+# Delete redis container
 function delete_redis_container()
 {
   docker rm redis-server
 }
 
+# Delete redis image
 function delete_redis_image()
 {
   docker rmi redis
 }
 
+# Build celery image
 function build_celery()
 {
     docker build -f ./Dockerfile.celery -t celery .
 }
 
+# Run celery image
 function run_celery()
 {
   docker run --name celery-worker --net=redis-celery --rm=false celery
-  # docker run --name celery-worker celery
 }
 
+# Start celery container
 function start_celery()
 {
   docker start celery-worker
 }
 
+# Stop celery container
 function stop_celery()
 {
   docker stop celery-worker
 }
 
+# Delete celery container
 function delete_celery_container()
 {
   docker rm celery-worker -f
 }
 
+# Delete celery image
 function delete_celery_image()
 {
   docker rmi celery -f
   docker rmi $(docker images -f dangling=true -q)
 }
 
+# Build wordcounting app
 function build_wordcounter()
 {
   docker build -f ./Dockerfile.wordcounter -t wordcounter .
 }
 
+# Run wordcounting app
 function run_wordcounter()
 {
   docker run -d -p 5000:5000 --name wordcounter_app --net=redis-celery --rm=false wordcounter
-  # docker run --name celery-worker celery
 }
 
 $@
--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from wordcounter.config import *
-from wordcounter import app
+from wordcounter.application import app
 
 # print Config.HOST
 app.run(host=DevelopmentConfig.HOST, debug=True)
