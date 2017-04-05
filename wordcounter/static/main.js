78672aba5711b8bfe7d197b33c96910dfdba561c
shwetank-kumar <shwetank.kumar@gmail.com>
2017-04-05 14:22:06 -0700

    First push Angular code included

--- wordcounter/static/main.js
+++ wordcounter/static/main.js
@@ -1,16 +1,23 @@
-(function () {
+.controller('WordcountController', ['$scope', '$log', '$http',
+  function($scope, $log, $http) {
 
-  'use strict';
+  $scope.getResults = function() {
 
-  angular.module('WordcountApp', [])
+    $log.log("test");
 
-  .controller('WordcountController', ['$scope', '$log',
-    function($scope, $log) {
-    $scope.getResults = function() {
-      $log.log("test");
-    };
-  }
+    // get the URL from the input
+    var userInput = $scope.url;
 
-  ]);
+    // fire the API request
+    $http.post('/start', {"url": userInput}).
+      success(function(results) {
+        $log.log(results);
+      }).
+      error(function(error) {
+        $log.log(error);
+      });
 
-}());
+  };
+
+}
+]);
